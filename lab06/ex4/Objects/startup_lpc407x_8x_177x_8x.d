1d017bb89e8942883c974538538790e875c2b7f4
Riccardo Cuccu <57985742+RiccardoCuccu@users.noreply.github.com>
2020-04-17 00:21:35 +0200

    Add files via upload

---
+++ lab06/ex4/Objects/startup_lpc407x_8x_177x_8x.d
@@ -0,0 +1,1 @@
+.\objects\startup_lpc407x_8x_177x_8x.o: RTE\Device\LPC4072FBD80\startup_LPC407x_8x_177x_8x.s
---
+++ lab06/ex4/Objects/system_lpc407x_8x_177x_8x.d
@@ -0,0 +1,10 @@
+.\objects\system_lpc407x_8x_177x_8x.o: RTE\Device\LPC4072FBD80\system_LPC407x_8x_177x_8x.c
+.\objects\system_lpc407x_8x_177x_8x.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\Keil\LPC4000_DFP\2.1.0\Device\Include\LPC407x_8x_177x_8x.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\system_lpc407x_8x_177x_8x.o: C:\Users\Riccardo\AppData\Local\Arm\Packs\Keil\LPC4000_DFP\2.1.0\Device\Include\system_LPC407x_8x_177x_8x.h
+.\objects\system_lpc407x_8x_177x_8x.o: RTE\Device\LPC4072FBD80\system_LPC407x_8x_177x_8x.h
