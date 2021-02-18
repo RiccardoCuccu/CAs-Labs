7f39ade66348155bb0340d5bd7e14f405b7f53ed
Riccardo <r.cuccu@outlook.it>
2021-02-18 12:06:19 +0100

    Update exam 2021-02-03

---
+++ exams/exam_20210203/README.md
@@ -0,0 +1,86 @@
+# Exam of 03.02.2021
+Exam of the Computer Architectures course of the master degree in Embedded System (Electronic Engineering) of Politecnico di Torino.<br/>
+Tested in Keil uVision5.<br/>
+
+## Exam Text
+
+### Specification 1 (? points)
+
+### Specification 2 (? points)
+
+### Specification 3 (? points)
+
+### Specification 4 (? points)
+
+Given a 4 x 4 matrix of WORD (i.e. 16 bits single data) SOURCE write a 8086 assembly program which rotates the rows of SOURCE from up to down by 1<=n<=3
+positions and stores the result in the matrix DESTINATION, with n given by the user. The choice is yours about how to store the matrices in the memory. Please add
+significant comments to the code and instructions. If you have time, in order to get one additional point, provide the instructions to extend the program to consider n in the range -80<=n<=+120
+
+#### Example:
+Initial matrix SOURCE
+```
+A B C D
+E F G H
+I J K L
+M N O P
+```
+if n=3 DESTINATION becomes
+```
+E F G H
+I J K L
+M N O P
+A B C D
+```
+if n=1 DESTINATION becomes
+```
+M N O P
+A B C D
+E F G H
+I J K L
+```
+
+### Specification 5 (9 points)
+
+The IEEE-754 SP standard expresses floating-point numbers in 32 bits:
+
+--- IMAGE ---
+
+Bit 31 is 0 if the number is positive, 1 if negative.
+
+Write the `addFPpositiveNumbers` subroutine, which receives in input two 32-bit numbers, considers them as IEEE-754 SP floating point numbers, and returns their
+sum (in the same format). Bit 31 of the two input numbers is always 0 (i.e., the two numbers are positive).
+
+In details, the subroutine implements the following steps:
+1. take the mantissa of the two parameters
+2. set the 23rd bit of the mantissa to 1
+3. compare the two exponents. If they are equal, the exponent of the result is the same. If they are different:
+	- the exponent of the result is the highest one
+	- shift right the mantissa of the number with the lower exponent by as many position as the difference between the two exponents.
+4. sum the two mantissas: this is the mantissa of the result. If the 24st bit of the mantissa of the result is 1:
+        - shift right the mantissa of the result by one position
+        - increment the exponent of the result by one.
+5. set the 23rd bit of the mantissa of the result to 0.
+6. combine the mantissa and the exponent to get the final result.
+
+#### Example
+`parameter1 = 0100 0010 0100 1011 0000 0000 0000 0000`
+`parameter2 = 0100 0001 1010 0100 0000 0000 0000 0000`
+
+1. `mantissa1 = 0000 0000 0100 1011 0000 0000 0000 0000`
+   `mantissa2 = 0000 0000 0010 0100 0000 0000 0000 0000`
+
+2. `mantissa1 = 0000 0000 1100 1011 0000 0000 0000 0000`
+   `mantissa2 = 0000 0000 1010 0100 0000 0000 0000 0000`
+
+3. `exponent1 = 1000 0100`
+   `exponent2 = 1000 0011`
+	`exponentResult = 1000 0100`
+	`mantissa2 = 0000 0000 0101 0010 0000 0000 0000 0000`
+
+4. `mantissaResult = 0000 0001 0001 1101 0000 0000 0000 0000`
+	`mantissaResult = 0000 0000 1000 1110 1000 0000 0000 0000`
+	`exponentResult = 1000 0101`
+
+5. `mantissaResult = 0000 0000 0000 1110 1000 0000 0000 0000`
+
+6. `result = 0100 0010 1000 1110 1000 0000 0000 0000`
---
+++ exams/exam_20210203/ex4/ex.asm
@@ -0,0 +1,151 @@
+; Exam of 03.02.2021
+
+        .model small
+        .stack
+
+        LEN         EQU 4
+        SIZE        EQU LEN*LEN
+
+        LF          EQU 10      ; line feed
+        CR          EQU 13      ; carriage return
+        PLUS        EQU 43      ; plus sign
+        MINUS       EQU 45      ; minus sign
+
+        .data
+
+        MSG         DB "Enter a value between -80 and +120:", CR, LF, "> ", "$"
+
+        SOURCE      DW 'A','B','C','D'
+                    DW 'E','F','G','H'
+                    DW 'I','J','K','L'
+                    DW 'M','N','O','P'
+                    
+        DESTINATION DW SIZE DUP(0)
+
+        N           DW  ?
+
+        .code
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+ROTATE  PROC                    ; ROTATE MATRIX
+        PUSHA
+
+        MOV     CX, LEN         ; set counter to LEN
+
+ROW:
+
+        MOV     DX, SOURCE[SI]
+        MOV     DESTINATION[DI], DX
+
+        ADD     SI, 2           ; increment source
+        ADD     DI, 2           ; increment destination
+
+        CMP     DI, 2*SIZE      ; check if the destination index has exceeded the size of the matrix
+        JE      RESETROT
+
+        LOOP    ROW
+        JMP     ENDROT
+
+RESETROT:
+        MOV     DI, 0           ; reset destination index
+        LOOP    ROW
+
+
+ENDROT:
+        POPA
+        RET
+ROTATE  ENDP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+INNUM   PROC                    ; INPUT NUMBER
+        PUSHA
+
+        MOV     N, 0            ; set N to 0
+        MOV     CH, 0
+
+NEXTNUM:
+        MOV     AH, 1           ; reading code
+        INT     21H             ; interrupt
+
+        CMP     AL, MINUS       ; check if the character is the minus sign
+        JE      NEGNUM 
+
+        CMP     AL, PLUS        ; check if the character is the plus sign
+        JE      NEXTNUM
+
+        CMP     AL, CR          ; check if the character is ENTER
+        JE      ENDSAVE
+
+        SUB     AL, 48          ; difference between ASCII and number
+        MOV     BL, AL          ; copy the number to BL
+        MOV     BH, 0           ; reset BH to have BX=BL
+
+        MOV     AX, N           ; copy the previously entered value into AX
+        MOV     CL, 10          ; prepare multiplication by 10
+        MUL     CL              ; multiply
+        ADD     AX, BX          ; add the value multiplied with the one just entered
+        MOV     N, AX           ; load the value to N
+        JMP     NEXTNUM
+
+ENDSAVE:
+        CMP     CH, 1           ; check if it is a negative number
+        JE      INCR            ; if it is it is necessary to remove the sign and add LEN/2
+        CMP     N, LEN          ; check if it is a number greater than LEN
+        JGE     REDUCE          ; if it is it is necessary to reduce it
+        JMP     ENDIN
+
+NEGNUM: 
+        MOV     CH, 1           ; flag negative number
+        JMP     NEXTNUM
+
+REDUCE: 
+        SUB     N, LEN          ; decrement N of LEN
+        JMP     ENDSAVE
+
+INCR:
+        MOV     CH, 0           ; removing the sign and incrementing by LEN/2 gives the same result as a positive number
+        ADD     N, SIZE/2
+        JMP     ENDSAVE
+
+ENDIN:
+        POPA
+        RET
+INNUM   ENDP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        .startup
+
+        LEA     DX, MSG         ; "Enter a value between 1 and 3:"
+        MOV     AH, 09H         ; printing code
+        INT     21H             ; interrupt
+
+        CALL    INNUM           ; get value N from user
+        MOV     AX, N           ; move value N to AX
+        MOV     SI, 0           ; set the source index to 0
+        MOV     DL, 2*LEN       ; prepare multiplication by 2*LEN
+        MUL     DL              ; multiply
+        MOV     DI, AX          ; move the destination index into DI
+        MOV     CX, LEN         ; set counter to LEN
+
+NEXT:   
+        CALL    ROTATE
+        ADD     SI, 2*LEN       ; increment source
+        ADD     DI, 2*LEN       ; increment destination
+        CMP     DI, 2*SIZE      ; check if the destination index has exceeded the size of the matrix
+        JE      RESNEXT
+
+        LOOP    NEXT
+        JMP     END
+
+RESNEXT:
+        MOV     DI, 0           ; reset destination index
+        LOOP    NEXT
+
+END:
+        .exit
+        end
