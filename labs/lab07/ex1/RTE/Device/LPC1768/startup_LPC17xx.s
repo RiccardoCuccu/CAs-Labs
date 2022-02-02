8571ed6b1259196169df53ef78cadaabfb71f335
Riccardo <r.cuccu@outlook.it>
2022-02-02 19:41:43 +0100

    New implementation

--- labs/lab07/ex1/RTE/Device/LPC1768/startup_LPC17xx.s
+++ labs/lab07/ex1/RTE/Device/LPC1768/startup_LPC17xx.s
@@ -120,6 +120,9 @@ CRP_Key         DCD     0xFFFFFFFF
                 AREA    |.text|, CODE, READONLY
 
 word            EQU     0x00000004                          ; 4 byte = 1 word
+;N               EQU     0x00000003                          ; rows of Matrix_A
+;M               EQU     0x00000004                          ; columns of Matrix_A and rows of Matrix_B
+;P               EQU     0x00000002                          ; columns of Matrix_B
 N               EQU     0x00000004                          ; rows of Matrix_A
 M               EQU     0x00000007                          ; columns of Matrix_A and rows of Matrix_B
 P               EQU     0x00000005                          ; columns of Matrix_B
@@ -139,9 +142,9 @@ col_iteration   EQU     P
 counter         RN       7
 h_partial_sum   RN       8
 l_partial_sum   RN       9
-element_A       RN      10
-element_B       RN      11
-element_C       RN      12
+address_A       RN      10
+address_B       RN      11
+address_C       RN      12
 
                 AREA    exercise, DATA, READWRITE
 Matrix_C        SPACE   NM_word
@@ -169,15 +172,26 @@ Matrix_B        DCD     0x00009088, 0xFFFE7578, 0x00000000, 0x0000E290, 0xFFFFB1
                 DCD     0x00014050, 0x00001388, 0x000084D0, 0xFFFFADF8, 0x000003E8
                 DCD     0x00011170, 0xFFFEFA48, 0x00002328, 0x00014050, 0x000036B0
 
+;Matrix_C        DCD      6,  23
+;                DCD     68, -63
+;                DCD     41,  20
+
+;Matrix_C        DCD     0x80000000, 0x1B7A4D40, 0x7FFFFFFF, 0x5E3C2540, 0x2D4CAE00
+;                DCD     0x7D1C32C0, 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF, 0x6164DC80
+;                DCD     0x7FFFFFFF, 0xBA5279C0, 0x80000000, 0x7FFFFFFF, 0x6A372980
+;                DCD     0x8D05CBC0, 0x7FFFFFFF, 0x80000000, 0x80000000, 0x80000000
+
 ; Reset Handler
 
 Reset_Handler   PROC
                 EXPORT  Reset_Handler             [WEAK]
-                LDR     element_C, =Matrix_C                ; load address of Matrix_C
-                LDR     element_A, =Matrix_A                ; load address of Matrix_A
-                LDR     element_B, =Matrix_B                ; load address of Matrix_B
+                
+                LDR     address_C, =Matrix_C                ; load address of Matrix_C
+                LDR     address_A, =Matrix_A                ; load address of Matrix_A
+                LDR     address_B, =Matrix_B                ; load address of Matrix_B
 
-                BL      col_cycle
+                ;BL      col_cycle                           ; slower/older implementation
+                BL      matrixCalc                          ; faster/newer implementation
 
                 NOP
                 NOP
@@ -186,49 +200,124 @@ Reset_Handler   PROC
 stop            B       stop
                 ENDP
 
+; Faster implementation
+
+matrixCalc      PROC
+                PUSH    {r1-r9, LR}
+                
+                EOR     r2, r2                              ; offset Matrix_A
+                EOR     r3, r3                              ; offset Matrix_B
+                EOR     r4, r4                              ; offset Matrix_C
+                EOR     r7, r7                              ; low partial result
+                EOR     r8, r8                              ; high partial result
+                MOV     r9, #M                              ; multiplication counter
+                
+                B       elementCalc                         ; jump to the calculation of the first destination element
+
+initializeR     ADD     r2, r2, #word                       ; move to the next row (Matrix_A)
+                EOR     r3, r3                              ; move to the first column (Matrix_B)
+                MOV     r9, #M                              ; set multiplication counter
+                B       elementCalc                         ; jump to the calculation of the next destination element
+
+initializeC     SUB     r3, r3, #MP_word-P_word-word        ; move to the next column (Matrix_B)
+                SUB     r2, r2, #M_word-word                ; move to the first element of the row (Matrix_A)
+                MOV     r9, #M                              ; set multiplication counter
+
+elementCalc     LDR     r0, [address_A, r2]                 ; load an element from Matrix_A
+                LDR     r1, [address_B, r3]                 ; load an element from Matrix_B
+
+                SMULL   r5, r6, r0, r1                      ; signed multiplication with 64-bit result [SMULL <RLo>, <RHi>, <Rn>, <Rm>]
+
+                ADDS    r7, r7, r5                          ; store low partial result setting carry
+                ADC     r8, r8, r6                          ; store high partial result adding carry
+                
+                SUBS    r9, r9, #1                          ; decrement multiplication counter
+                ITTT    NE                                  ; check if it is zero
+                ADDNE   r2, r2, #word                       ; if it is not zero move to the next row element (Matrix_A)
+                ADDNE   r3, r3, #P_word                     ; if it is not zero move to the next column element (Matrix_B)
+                BNE     elementCalc                         ; if it is not zero jump to the calculation of the next destination element
+                
+                TEQ     r8, #0x00000000                     ; high partial result equivalence test with all zeros (positive overflow check)
+                BEQ     positiveHalf                        ; jump to the check of the sign of the low partial result
+                
+                TEQ     r8, #0xFFFFFFFF                     ; high partial result equivalence test with all ones (negative overflow check)
+                BEQ     negativeHalf                        ; jump to the check of the sign of the low partial result
+                
+                TST     r8, #0x80000000                     ; check if the MSB of the high partial result is set
+                ITE     EQ                                  ; if the MSB of the high partial result is set means that the result is not zero (Z=0)
+                MOVEQ   r7, #0x7FFFFFFF                     ; Z=1 means MSB=0 (positive number)
+                MOVNE   r7, #0x80000000                     ; Z=0 means MSB=1 (negative number)
+                
+                B       elementStore                        ; jump to store the element
+                
+positiveHalf    TST     r7, #0x80000000                     ; test the higher bit of the low partial result
+                BEQ     elementStore                        ; jump to store the element if test result is zero
+                MOV     r7, #0x7FFFFFFF                     ; otherwise use the highest possible value
+                B       elementStore                        ; jump to store the element
+
+negativeHalf    TST     r7, #0x80000000                     ; test the higher bit of the low partial result
+                BNE     elementStore                        ; jump to store the element if test result is not zero
+                MOV     r7, #0x80000000                     ; otherwise use the lowest possible value 
+
+elementStore    STR     r7, [address_C, r4]                 ; store the element to Matrix_C
+                EOR     r7, r7                              ; reset low partial result
+                EOR     r8, r8                              ; reset high partial result
+                ADD     r4, r4, #4                          ; move to the next element (Matrix_C)
+                
+                CMP     r3, #MP_word-word                   ; check if it is the last element of the last column (Matrix_B)
+                BNE     initializeC                         ; if not jump to the initialization of a new column
+                
+                CMP     r2, #NM_word-word                   ; check if it is the last element of the last row (Matrix_A)
+                BNE     initializeR                         ; if not jump to the initialization of a new row
+                
+                POP     {r1-r9, PC}
+                ENDP
+
+; Slower implementation
+
 col_cycle       PROC
-                PUSH    {element_A, counter, LR}
+                PUSH    {address_A, counter, LR}
                 MOV     h_partial_sum, #0                   ; reset high partial sum
                 MOV     l_partial_sum, #0                   ; reset low partial sum
                 MOV     counter, #row_iteration             ; load the number of rows of Marix_C
 col_items       BL      row_cycle
-                ADD     element_A, element_A, #M_word       ; next row of Marix_A (new row)
+                ADD     address_A, address_A, #M_word       ; next row of Marix_A (new row)
                 SUB     counter, counter, #1                ; decrement counter
                 CMP     counter, #0                         ; check if counter is #0
                 BNE     col_items                           ; return to column items if count is not #0
-                POP     {element_A, counter, PC}            ; return
+                POP     {address_A, counter, PC}            ; return
                 ENDP
 
 row_cycle       PROC
-                PUSH    {element_B, counter, LR}
+                PUSH    {address_B, counter, LR}
                 MOV     counter, #col_iteration             ; load the number of colums of Marix_C
 row_items       BL      sum_elements
-                ADD     element_B, element_B, #word         ; next element of Marix_B (new column)
+                ADD     address_B, address_B, #word         ; next element of Marix_B (new column)
                 SUB     counter, counter, #1                ; decrement counter
                 CMP     counter, #0                         ; check if counter is #0
                 BNE     row_items                           ; return to row items if count is not #0
-                POP     {element_B, counter, PC}            ; return
+                POP     {address_B, counter, PC}            ; return
                 ENDP
 
 sum_elements    PROC
-                PUSH    {element_A, element_B, h_partial_sum, l_partial_sum, counter, LR}
+                PUSH    {address_A, address_B, h_partial_sum, l_partial_sum, counter, LR}
                 MOV     counter, #sum_iteration             ; load the number of sums
 sum_items       BL      mul_elements                        ; call mul_elements
-                ADD     element_A, element_A, #word         ; next element of Marix_A (new column)
-                ADD     element_B, element_B, #P_word       ; next element of Marix_B (new row)
+                ADD     address_A, address_A, #word         ; next element of Marix_A (new column)
+                ADD     address_B, address_B, #P_word       ; next element of Marix_B (new row)
                 SUB     counter, counter, #1                ; decrement counter
                 CMP     counter, #0                         ; check if counter is #0
                 BNE     sum_items                           ; return to sum items if count is not #0
                 BL      check_result                        ; check overflow
-                STR     l_partial_sum, [element_C]          ; store result
-                ADD     element_C, element_C, #word         ; next element of Marix_C
-                POP     {element_A, element_B, h_partial_sum, l_partial_sum, counter, PC}   
+                STR     l_partial_sum, [address_C]          ; store result
+                ADD     address_C, address_C, #word         ; next element of Marix_C
+                POP     {address_A, address_B, h_partial_sum, l_partial_sum, counter, PC}   
                 ENDP
 
 mul_elements    PROC
                 PUSH    {counter, LR}
-                LDR     r0, [element_A]                     ; load value from Matrix_A
-                LDR     r1, [element_B]                     ; load value from Matrix_B
+                LDR     r0, [address_A]                     ; load value from Matrix_A
+                LDR     r1, [address_B]                     ; load value from Matrix_B
                 SMULL   r2, r3, r0, r1                      ; signed multiplication with 64-bit result [SMULL <RLo>, <RHi>, <Rn>, <Rm>]
                 ADDS    l_partial_sum, l_partial_sum, r2    ; store low partial result setting carry
                 ADCS    h_partial_sum, h_partial_sum, r3    ; store high partial result adding carry
